5e7fe7e194b511dc6e453526f53bd31b72b20b84
EnChyb <144510772+EnChyb@users.noreply.github.com>
2024-11-05 21:51:08 +0100

    hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,111 @@
-const express = require('express')
+const express = require('express');
+const { listContacts, getContactById, addContact, removeContact, updateContact } = require('../../models/contacts');
+const Joi = require('joi');
+
 
 const router = express.Router()
 
+const schema = Joi.object({
+  name: Joi.string().alphanum().min(2).required(),
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  phone: Joi.required(),
+})
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await listContacts();
+  console.log(contacts)
+  res.json({
+    message: 'Contacts list',
+    contacts
+  
+  })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId
+  console.log('received ID:', id)
+  const contactById = await getContactById(id)
+  if (!contactById) {
+    console.log('if works')
+    return res.status(404).json({
+      message: `Contact with ID ${id} doesn't exist`
+    });
+  }
+  console.log('ominięty if')
+  res.json({
+    message: `Contact with ID ${id} founded succesfully`,
+    contactById
+  })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const body = await req.body;
+  console.log('Request body:', body)
+  const validatedBody = schema.validate(body)
+  console.log(validatedBody)
+  try {
+    const newContact = await addContact(validatedBody);
+
+    if (validatedBody.error) {
+      res.status(400).json({message: validatedBody.error.message})
+      throw new Error('Failed to add contact');
+    }
+
+
+    res.status(201).json({
+      message: 'New contact is succesfully created!',
+      newContact
+
+    })
+  } catch (error) {
+    console.log('Router error:', error)
+  }
+
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+  try {
+    const id = req.params.contactId
+    console.log('received ID:', id)
+    const newContacts = await removeContact(id)
+
+    if (!newContacts) {
+    return res.status(404).json({
+      message: `Contact with ID ${id} not found`
+    });
+    }
+    res.json({ message: 'contact deleted' })
+  
+  } catch (error) {
+    console.log(error)
+    
+  }
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const id = req.params.contactId
+    console.log('received ID:', id)
+
+    const body = await req.body;
+    const validatedBody = schema.validate(body)
+
+    if (validatedBody.error) {
+      res.status(404).json({message: validatedBody.error.message})
+      throw new Error('Failed to update contact- missing fields');
+    }
+
+    updateContact(id, validatedBody)
+
+    res.json({ message: 'contact updated succesfully' })
+    
+  } catch (error) {
+    console.log(error)
+    
+  }
+
 })
 
 module.exports = router
